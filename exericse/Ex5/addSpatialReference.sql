07c572ec93223f157063c47cac6303246db9f7aa
clb2001 <clb245000@163.com>
2023-07-02 23:55:08 +0800

    2023.7.2

---
+++ exericse/Ex5/addSpatialReference.sql
@@ -0,0 +1,6 @@
+insert into spatial_ref_sys 
+values(101,'POSC',32214,'PROJCS[" UTM_ZONE_14N",GEOGCS[" World Geodetic System 72",
+DATUM["WGS_72",ELLIPSOID["NWL_10D",6378135,298.26"]],PRIMEN[" Greenwich",0],UNIT[" Meter",1.0]],
+PROJECTION["Transverse_Mercator"],PARAMETER[" False_Easting",500000.0],PARAMETER[" False_Northing",
+0.0],PARAMETER[" Central_Meridian",- 99.0],PARAMETER[" Scale_Factor",0.9996],PARAMETER["Latitude_of_origin",
+0.0],UNIT[" Meter",1.0]]');
